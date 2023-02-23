57b5502cb012272abcf268f00ae1c56e796fb844
Doug Bleek <dougbleek@gmail.com>
2023-02-22 22:10:26 -0500

    "Overwriteable edits to urls and views"

--- voluncheer/registration/urls.py
+++ voluncheer/registration/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 app_name = 'registration'
 urlpatterns = [
+    path('', views.registration, name='registration'),
 ]
--- voluncheer/registration/views.py
+++ voluncheer/registration/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# ======================== Registration ============================
+def registration(request):
+  context = {}
+  return render(request, 'voluncheer/registration.html', context)
