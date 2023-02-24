d30eda6658a1c4759585594611f66e1374fbccbc
Doug Bleek <dougbleek@gmail.com>
2023-02-23 23:42:17 -0500

    "Rebuilt db and created custom user model"

--- voluncheer/registration/models.py
+++ voluncheer/registration/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 ROLE_CHOICES = (
-    (1,'Volunteer'),
-    (2,'Organization'),
+    ('volunteer', 'Volunteer'),
+    ('organization','Organization'),
 )
 
 class User(AbstractUser):
@@ -14,10 +14,12 @@ class User(AbstractUser):
     date_of_birth = models.DateField(blank = True, null = True) 
     email = models.EmailField(max_length = 254, unique = True)
     password = models.CharField(max_length = 127)
+    role = models.CharField(max_length = 12, choices = ROLE_CHOICES, default = 'volunteer')
 
-    is_admin = models.BooleanField(default = False)
+    REQUIRED_FIELDS = [role]
 
-    role = models.PositiveSmallIntegerField(choices = ROLE_CHOICES)
+    class Meta:
+        db_table = 'auth_user'
 
     def __str__(self):
         return self.last_name + ", " + self.first_name
