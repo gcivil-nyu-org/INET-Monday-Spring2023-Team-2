37cc00377980a95ad7e18be0e3bddacdf909d4eb
Wayne Zhu <42898661+JinweiZhu@users.noreply.github.com>
2023-03-14 18:23:06 -0400

    Update voluncheer/voluncheer/settings.py
    Co-authored-by: Joe Leuenberger <joeleu@users.noreply.github.com>

--- voluncheer/voluncheer/settings.py
+++ voluncheer/voluncheer/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 MEDIA_URL = "/media/"
 
 # Quick-start development settings - unsuitable for production
