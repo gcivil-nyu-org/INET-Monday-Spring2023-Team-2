c0c1f9f066635a7acde2e6e4f94333688aaaeb0d
dbleek <36943441+dbleek@users.noreply.github.com>
2023-03-14 20:59:57 -0400

    Cleaned up urls.py in project root folder

--- voluncheer/profiles/urls.py
+++ voluncheer/profiles/urls.py
@@ -2,11 +2,25 @@ from django.urls import path
 from django.urls import re_path
 
 from profiles.views.home import home
+from profiles.views.home import SignUpView
+from profiles.views.organizations import OrganizationSignUpView
 from profiles.views.profile import ProfileView
 from profiles.views.profile import profile_update
+from profiles.views.volunteers import VolunteerSignUpView
 
 urlpatterns = [
     path("", home, name="home"),
     path(r"^profile/$", ProfileView.as_view(), name="profile"),
     path(r"^profile/update/$", profile_update, name="profile_update"),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path(
+        "signup/organization/",
+        OrganizationSignUpView.as_view(),
+        name="organization_signup",
+    ),
+    path(
+        "signup/volunteer/",
+        VolunteerSignUpView.as_view(),
+        name="volunteer_signup",
+    ),
 ]
