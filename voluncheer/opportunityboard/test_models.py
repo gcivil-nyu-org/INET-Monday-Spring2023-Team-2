0500f1095898f5296d84de3cefabdc5f502e1c35
dbleek <36943441+dbleek@users.noreply.github.com>
2023-03-19 09:02:43 -0400

    Added additional models tests

--- voluncheer/opportunityboard/test_models.py
+++ voluncheer/opportunityboard/test_models.py
@@ -4,6 +4,8 @@ from django.test import TestCase
 
 from opportunityboard.models import Category
 from opportunityboard.models import Opportunity
+from opportunityboard.models import Subcategory
+from opportunityboard.models import Subsubcategory
 from profiles.models import Organization
 from profiles.models import User
 from profiles.models import UserType
@@ -12,9 +14,10 @@ from profiles.models import UserType
 class OpportunityTest(TestCase):
     """Test cases for Opportunity model"""
 
-    def setUp(self):
+    @classmethod
+    def setUpTestData(cls):
         """See base class."""
-        self.jedi = Organization.objects.create(
+        cls.jedi = Organization.objects.create(
             user=User.objects.create(
                 email="jedi@jedi.com",
                 password="peace_and_justice_for_the_galaxy",
@@ -23,10 +26,13 @@ class OpportunityTest(TestCase):
             name="Jedi Council",
         )
 
-    def test_opportunity_details(self):
-        """Test basic opportunity details"""
+        cls.environment = Category.objects.create(name="Environment")
 
-        category = Category.objects.create(name="Environment")
+        cls.conservation = Subcategory.objects.create(name="Conservation", parent=cls.environment)
+
+        cls.reforestation = Subsubcategory.objects.create(
+            name="Reforestation", parent=cls.conservation
+        )
 
         description = (
             "Please help us support our community at this week's" "Cloud City soup kitchen"
@@ -40,9 +46,11 @@ class OpportunityTest(TestCase):
             minute=0,
         )
 
-        soup = Opportunity.objects.create(
-            organization=self.jedi,
-            category=category,
+        cls.soup = Opportunity.objects.create(
+            organization=cls.jedi,
+            category=cls.environment,
+            subcategory=cls.conservation,
+            subsubcategory=cls.reforestation,
             title="Cloud City Soup Kitchen",
             description=description,
             date=a_date,
@@ -55,15 +63,76 @@ class OpportunityTest(TestCase):
             is_published=False,
         )
 
-        self.assertEqual(soup.organization.name, "Jedi Council")
-        self.assertEqual(soup.category, category)
-        self.assertEqual(soup.title, "Cloud City Soup Kitchen")
-        self.assertEqual(soup.description, description)
-        self.assertEqual(soup.date, a_date)
-        self.assertEqual(soup.end, end)
-        self.assertEqual(soup.address_1, "200 Calrissian Av.")
-        self.assertEqual(soup.address_2, "NY")
-        self.assertEqual(soup.longitude, 12.34)
-        self.assertEqual(soup.latitude, 56.78)
-        self.assertEqual(soup.staffing, 9)
-        self.assertFalse(soup.is_published)
+    def test_opportunity_details(self):
+        """Test basic opportunity details"""
+        self.assertEqual(self.soup.organization.name, "Jedi Council")
+        self.assertEqual(self.soup.category.name, "Environment")
+        self.assertEqual(self.soup.subcategory.name, "Conservation")
+        self.assertEqual(self.soup.subsubcategory.name, "Reforestation")
+        self.assertEqual(self.soup.title, "Cloud City Soup Kitchen")
+        self.assertEqual(
+            self.soup.description,
+            "Please help us support our community at this week's" "Cloud City soup kitchen",
+        )
+        self.assertEqual(
+            self.soup.date,
+            datetime.datetime(
+                year=2023,
+                month=3,
+                day=9,
+                hour=18,
+                minute=0,
+            ),
+        )
+        self.assertEqual(self.soup.end, "12:00:00")
+        self.assertEqual(self.soup.address_1, "200 Calrissian Av.")
+        self.assertEqual(self.soup.address_2, "NY")
+        self.assertEqual(self.soup.longitude, 12.34)
+        self.assertEqual(self.soup.latitude, 56.78)
+        self.assertEqual(self.soup.staffing, 9)
+        self.assertFalse(self.soup.is_published)
+
+    def test_category_details(self):
+        """Test basic category details"""
+        category = self.environment
+        self.assertEqual(category.name, "Environment")
+
+    def test_subcategory_details(self):
+        """Test basic subcategory details"""
+        subcategory = self.conservation
+        self.assertEqual(subcategory.name, "Conservation")
+        self.assertEqual(subcategory.parent.name, "Environment")
+
+    def test_subsubcategory_details(self):
+        """Test basic subsubcategory details"""
+        subsubcategory = self.reforestation
+        self.assertEqual(subsubcategory.name, "Reforestation")
+        self.assertEqual(subsubcategory.parent.name, "Conservation")
+
+    def test_opportunity_object_name_is_title(self):
+        """Tests Opportunity __str__ method returns title"""
+        opportunity = Opportunity.objects.get(id=1)
+        expected_opportunity_name = f"{opportunity.title}"
+        self.assertEqual(str(opportunity), expected_opportunity_name)
+
+    def test_category_object_name_is_name(self):
+        """Tests Category __str__ method returns name"""
+        category = Category.objects.get(id=1)
+        expected_category_name = f"{category.name}"
+        self.assertEqual(str(category), expected_category_name)
+
+    def test_subcategory_object_name_is_full_path(self):
+        """Tests Subcategory __str__ method returns full path"""
+        subcategory = Subcategory.objects.get(id=1)
+        expected_subcategory_name = "{0} -> {1}".format(subcategory.parent.name, subcategory.name)
+        self.assertEqual(str(subcategory), expected_subcategory_name)
+
+    def test_subsubcategory_object_name_is_full_path(self):
+        """Tests Subsubcategory __str__ method returns full path"""
+        subsubcategory = Subsubcategory.objects.get(id=1)
+        expected_subsubcategory_name = "{0} -> {1} -> {2}".format(
+            subsubcategory.parent.parent.name,
+            subsubcategory.parent.name,
+            subsubcategory.name,
+        )
+        self.assertEqual(str(subsubcategory), expected_subsubcategory_name)
