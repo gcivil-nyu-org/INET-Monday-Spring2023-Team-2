c20cc043b3748093fb47e26d32fafa21f56d97b5
Hgersten-hash <70909790+Hgersten-hash@users.noreply.github.com>
2023-03-21 22:30:03 -0400

    Fixed domain for localhost

--- voluncheer/voluncheer/urls.py
+++ voluncheer/voluncheer/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
-from django.urls import re_path
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.urls import re_path
 from django.views.static import serve
 
 from profiles.views.home import SignUpView
