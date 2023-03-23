05cad03542939223ae45d24669a6c7ce7e16d855
JinweiZhu <jz3395@nyu.edu>
2023-03-22 23:28:11 -0400

    test passed. Now fix no profile photo opportunity bug

--- voluncheer/nuke-and-rebuild-db.sh
+++ voluncheer/nuke-and-rebuild-db.sh
@@ -3,7 +3,7 @@
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 rm db.sqlite3
-pip3 uninstall -y Django
-pip3 install Django==3.2.8
+#pip uninstall -y Django
+#pip install Django==3.2.8
 ./manage.py makemigrations
 ./manage.py migrate
