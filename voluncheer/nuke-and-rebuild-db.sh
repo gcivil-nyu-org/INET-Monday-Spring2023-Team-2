9c0adbf054507b797c2b8e9a103087bc94c320b6
JinweiZhu <jz3395@nyu.edu>
2023-03-23 18:51:57 -0400

    done

--- voluncheer/nuke-and-rebuild-db.sh
+++ voluncheer/nuke-and-rebuild-db.sh
@@ -2,8 +2,17 @@
 #AKA big red button
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
+find . -path "*/migrations/*pycache*"  -delete
 rm db.sqlite3
 #pip uninstall -y Django
 #pip install Django==3.2.8
+git checkout origin/develop -- db.sqlite3
+mv db.sqlite3 dbTemp.sqlite3
 ./manage.py makemigrations
 ./manage.py migrate
+#Dump data
+sqlite3 dbTemp.sqlite3 ".dump opportunityboard_category" |sqlite3 db.sqlite3
+sqlite3 dbTemp.sqlite3 ".dump opportunityboard_opportunity" |sqlite3 db.sqlite3
+sqlite3 dbTemp.sqlite3 ".dump opportunityboard_subcategory" |sqlite3 db.sqlite3
+sqlite3 dbTemp.sqlite3 ".dump opportunityboard_subsubcategory" |sqlite3 db.sqlite3
+rm dbTemp.sqlite3
