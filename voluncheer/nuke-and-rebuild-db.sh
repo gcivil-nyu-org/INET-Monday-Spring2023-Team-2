377c4b6c5fd1f5c0ee9615a72b52115fead3fb81
JinweiZhu <jz3395@nyu.edu>
2023-03-26 14:30:17 -0400

    automate category data scripting for convenient local and CI usage

--- voluncheer/nuke-and-rebuild-db.sh
+++ voluncheer/nuke-and-rebuild-db.sh
@@ -4,15 +4,10 @@ find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 find . -path "*/migrations/*pycache*"  -delete
 rm db.sqlite3
+#This might be needed if django error occurs.
 #pip uninstall -y Django
 #pip install Django==3.2.8
-git checkout origin/develop -- db.sqlite3
-mv db.sqlite3 dbTemp.sqlite3
 ./manage.py makemigrations
 ./manage.py migrate
-#Dump data
-sqlite3 dbTemp.sqlite3 ".dump opportunityboard_category" |sqlite3 db.sqlite3
-sqlite3 dbTemp.sqlite3 ".dump opportunityboard_opportunity" |sqlite3 db.sqlite3
-sqlite3 dbTemp.sqlite3 ".dump opportunityboard_subcategory" |sqlite3 db.sqlite3
-sqlite3 dbTemp.sqlite3 ".dump opportunityboard_subsubcategory" |sqlite3 db.sqlite3
-rm dbTemp.sqlite3
+#Load the data for categories.
+./manage.py loaddata opportunityboard/fixtures/category_data.json
