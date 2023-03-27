6ab60822e9a15fac564bbb510fabe1e85c528398
Hgersten-hash <70909790+Hgersten-hash@users.noreply.github.com>
2023-03-26 22:21:40 -0400

    Added comments for bin_num

--- voluncheer/map/models.py
+++ voluncheer/map/models.py
@@ -11,6 +11,8 @@ class NYCharities(models.Model):
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
     type = models.CharField(max_length=255)
+    # use bin number of the ny_charity as an additional identifier for an ny_charity entry
+    # see data set: https://data.cityofnewyork.us/Social-Services/2019-Volunteers-Count-Report-Boroughs/yunp-vs8g
     bin_num = models.PositiveBigIntegerField(default=0)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
