6ef7531f05798ebb5b139a911a2a9777b4dafbb9
Hanna Gersten <70909790+Hgersten-hash@users.noreply.github.com>
2023-03-27 11:25:49 -0400

    Nyc open data (#126)
    * Fixed domain for localhost
    
    * Fixed domain for localhost
    
    * Fixed domain for localhost
    
    * Fixed domain for localhost
    
    * Fixed domain for localhost
    
    * Fixed domain for localhost
    
    * Fixed domain for localhost
    
    * Pulled nyc data
    
    * Pulled nyc data
    
    * Pulled nyc data
    
    * Pulled nyc data
    
    * Added tests for command
    
    * Added tests for command
    
    * Reset database
    
    * Addressed pr changes
    
    * Changed float to decimal
    
    * Changed fields for lat/long
    
    * Changed fields for lat/long
    
    * Updated command to allow address changes
    
    * Updated command to allow address changes
    
    * Changed to update_or_create
    
    * Excluded db
    
    * Added comments for bin_num
    
    * Added comments for bin_num

---
+++ voluncheer/map/management/commands/pull_nyc_charities.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
+from django.db import transaction
+import requests
+
+from map.models import NYCharities
+
+
+class Command(BaseCommand):
+    help = "Pull data from NYC Open Data and save to NYCharities table"
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        url = "https://data.cityofnewyork.us/resource/yunp-vs8g.json"
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            raise CommandError("Could not pull data from NYC Open Data")
+
+        data = response.json()
+        for row in data:
+            # Create a new NYCharities object for each item in the dataset
+            try:
+                nycharity, created = NYCharities.objects.update_or_create(
+                    name=row["organization_city_agency"],
+                    type=row["organization_type"],
+                    bin_num=row["bin"],
+                    defaults={
+                        "street": row["street_address_mailing_address"],
+                        "city": row["city"],
+                        "state": row["state"],
+                        "latitude": row["latitude"],
+                        "longitude": row["longitude"],
+                    },
+                )
+
+            except KeyError:
+                pass
