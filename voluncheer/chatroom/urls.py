fdfa48246f25c3d5e54055fbb9a16f4390d905f6
JinweiZhu <jz3395@nyu.edu>
2023-03-29 19:43:08 -0400

    chat page showing up again

--- voluncheer/chatroom/urls.py
+++ voluncheer/chatroom/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import re_path,path, include
 from chatroom.views import chatroom
 urlpatterns = [
-    re_path("profile/<int:user_id>/chatroom", chatroom, name="chatroom"),
+    path("", chatroom, name="chatroom"),
 ]
