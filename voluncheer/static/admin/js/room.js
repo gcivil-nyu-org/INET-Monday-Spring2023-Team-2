c62d5682b209477b0776217bad4b240f35b8c56b
Wayne Zhu <42898661+JinweiZhu@users.noreply.github.com>
2023-04-07 16:34:07 -0400

    Chatroom (#158)
    * initial commit
    
    * chat page showing up again
    
    * prelimary real-time chatroom done
    
    * remove docker from requirements.txt becasue it's not supposed to be installed by pip
    
    * reformatted
    
    * move channels's location
    
    * format
    
    * need python3.9 and docker to run
    
    * added python 3.9
    
    * python version added
    
    * spin up docker
    
    * remove extra install docker
    
    * assume docker already installed
    
    * format changes
    
    * override_settings decorator not working in test
    
    * fix some tests and format
    
    * Fix the chatroom tests.
    
    * fix the rest
    
    * fix format
    
    * fix issues and format
    
    * change port name
    
    * minor change is_aws
    
    ---------
    
    Co-authored-by: Joe Leuenberger <joeleu@users.noreply.github.com>

---
+++ voluncheer/static/admin/js/room.js
@@ -0,0 +1,82 @@
+const roomName = JSON.parse(document.getElementById('roomName').textContent);
+
+let chatLog = document.querySelector("#chatLog");
+let chatMessageInput = document.querySelector("#chatMessageInput");
+let chatMessageSend = document.querySelector("#chatMessageSend");
+let onlineUsersSelector = document.querySelector("#onlineUsersSelector");
+
+// adds a new option to 'onlineUsersSelector'
+function onlineUsersSelectorAdd(value) {
+    if (document.querySelector("option[value='" + value + "']")) return;
+    let newOption = document.createElement("option");
+    newOption.value = value;
+    newOption.innerHTML = value;
+    onlineUsersSelector.appendChild(newOption);
+}
+
+// removes an option from 'onlineUsersSelector'
+function onlineUsersSelectorRemove(value) {
+    let oldOption = document.querySelector("option[value='" + value + "']");
+    if (oldOption !== null) oldOption.remove();
+}
+
+// focus 'chatMessageInput' when user opens the page
+chatMessageInput.focus();
+
+// submit if the user presses the enter key
+chatMessageInput.onkeyup = function (e) {
+    if (e.keyCode === 13) {  // enter key
+        chatMessageSend.click();
+    }
+};
+
+// clear the 'chatMessageInput' and forward the message
+chatMessageSend.onclick = function () {
+    if (chatMessageInput.value.length === 0) return;
+    chatSocket.send(JSON.stringify({
+        "message": chatMessageInput.value,
+    }));
+    chatMessageInput.value = "";
+};
+
+let chatSocket = null;
+
+function connect() {
+    chatSocket = new WebSocket("ws://" + window.location.host + "/ws/chat/" + roomName + "/");
+
+    chatSocket.onopen = function (e) {
+        console.log("Successfully connected to the WebSocket.");
+    }
+
+    chatSocket.onclose = function (e) {
+        console.log("WebSocket connection closed unexpectedly. Trying to reconnect in 2s...");
+        setTimeout(function () {
+            console.log("Reconnecting...");
+            connect();
+        }, 2000);
+    };
+
+    chatSocket.onmessage = function (e) {
+        const data = JSON.parse(e.data);
+        console.log(data);
+
+        switch (data.type) {
+            case "chat_message":
+                chatLog.value += data.message + "\n";
+                break;
+            default:
+                console.error("Unknown message type!");
+                break;
+        }
+
+        // scroll 'chatLog' to the bottom
+        chatLog.scrollTop = chatLog.scrollHeight;
+    };
+
+    chatSocket.onerror = function (err) {
+        console.log("WebSocket encountered an error: " + err.message);
+        console.log("Closing the socket.");
+        chatSocket.close();
+    }
+}
+connect();
