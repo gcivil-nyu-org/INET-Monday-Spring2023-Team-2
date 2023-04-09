1b5e470ccdad5582520080d35eced6b09a03685f
dbleek <36943441+dbleek@users.noreply.github.com>
2023-04-09 00:25:12 -0400

    changes from code review/updates for readability

--- voluncheer/profiles/management/commands/load_badge_data.py
+++ voluncheer/profiles/management/commands/load_badge_data.py
@@ -23,20 +23,27 @@ def parse_time(time_str):
 
 
 class Command(BaseCommand):
-    @transaction.atomic
+    help = "Loads the missing default volunteer badges."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--badge_data",
+            default="profiles/fixtures/badge_data.json",
+            help="A JSON with the default badge data",
+        )
+
     def handle(self, *args, **options):
+        del args  # Unused.
         json_data = open("profiles/fixtures/badge_data.json")
         data = json.load(json_data)
 
-        for row in data:
-            # Create a new Badge object for each item in the dataset
-            try:
-                badge, created = Badge.objects.update_or_create(
-                    name=row["fields"]["name"],
-                    type=row["fields"]["type"],
-                    img=row["fields"]["img"],
-                    hours_required=parse_time(row["fields"]["hours_required"]),
-                )
-
-            except KeyError:
-                pass
+        for entry in data:
+            # Create a new Badge object for each entry in the dataset
+            name = entry["fields"]["name"]
+            badge, created = Badge.objects.update_or_create(
+                name=name,
+                type=entry["fields"]["type"],
+                img=entry["fields"]["img"],
+                hours_required=parse_time(entry["fields"]["hours_required"]),
+            )
+            self.stdout.write(self.style.SUCCESS(f"Successfully created {repr(name)}."))
