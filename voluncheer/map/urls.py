97610ebfa2fca1c8351af8c4a69a5048daa97125
suryasdas <47587516+suryasdas@users.noreply.github.com>
2023-04-10 17:55:20 -0400

    requested reviews

--- voluncheer/map/urls.py
+++ voluncheer/map/urls.py
@@ -4,5 +4,4 @@ from map import views
 
 urlpatterns = [
     path("", views.map, name="map"),
-    # path("",views.search_address, name ="search_address")
 ]
--- voluncheer/map/views.py
+++ voluncheer/map/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# from django.conf import settings
 from map import models
 from voluncheer import settings
 
