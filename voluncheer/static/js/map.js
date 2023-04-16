7307178b3a7a90d67ed67558fb689f7885e2cdfd
suryasdas <47587516+suryasdas@users.noreply.github.com>
2023-04-16 19:45:16 -0400

    updated map.js and map.html
    updated the code for map.js and map.html without updating the code for formatting all files and changing no migrations.

--- voluncheer/static/js/map.js
+++ voluncheer/static/js/map.js
@@ -10,11 +10,57 @@ async function initMap() {
     center: nyc,
     zoom: 11,
     mapId: "8e91118109e26fc7",
+    mapTypeControl: true,
   });
 
   const organizations = JSON.parse(
     document.getElementById("organizations").textContent
   );
+
+  const input = document.getElementById("address");
+  const autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo("bounds", map);
+
+  const infowindow = new google.maps.InfoWindow();
+  const infowindowContent = document.getElementById("infowindow-content");
+
+  infowindow.setContent(infowindowContent);
+
+  const marker = new google.maps.Marker({
+    map,
+    anchorPoint: new google.maps.Point(0, -29),
+  });
+
+  autocomplete.addListener("place_changed", () => {
+    infowindow.close();
+    marker.setVisible(false);
+
+    const place = autocomplete.getPlace();
+
+    if (!place.geometry || !place.geometry.location) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert(
+        "No details available for input: '" + place.name + "'"
+      );
+      return;
+    }
+
+  // If the place has a geometry, then present it on a map.
+  if (place.geometry.viewport) {
+    map.fitBounds(place.geometry.viewport);
+  } else {
+    map.setCenter(place.geometry.location);
+    map.setZoom(15);
+  }
+
+  marker.setPosition(place.geometry.location);
+  marker.setVisible(true);
+  infowindowContent.children["place-name"].textContent = place.name;
+  infowindowContent.children["place-address"].textContent =
+    place.formatted_address;
+  infowindow.open(map, marker);
+});
 
   const total = organizations.length;
   const observer = new IntersectionObserver((entries) => {
