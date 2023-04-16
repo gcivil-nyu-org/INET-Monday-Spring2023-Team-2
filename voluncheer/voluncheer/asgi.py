509967eb485101543fd7316d1b071648250ba3f6
JinweiZhu <jz3395@nyu.edu>
2023-04-15 21:00:16 -0400

    done

--- voluncheer/voluncheer/asgi.py
+++ voluncheer/voluncheer/asgi.py
@@ -7,23 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter
+from channels.routing import URLRouter
 import django
+from django.core.asgi import get_asgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voluncheer.settings")
 django.setup()
 
-from channels.routing import ProtocolTypeRouter
-from channels.routing import URLRouter
-from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-
-
-django_asgi_app=get_asgi_application()
-
+django_asgi_app = get_asgi_application()
 from chatroom import routing
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
     }
 )
